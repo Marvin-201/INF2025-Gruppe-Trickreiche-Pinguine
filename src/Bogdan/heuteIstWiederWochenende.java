ad14e1ef87acf65e64aa54ac42ae4608d4487cf5
Bogdan187 <saei.bogdan@gmail.com>
2026-06-12 11:43:12 +0200

    Noch ein Test

--- src/Bogdan/heuteIstWiederWochenende.java
+++ src/Bogdan/heuteIstWiederWochenende.java
@@ -3,7 +3,7 @@ package Bogdan;
 public class heuteIstWiederWochenende {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Durchlauf der Möhren: " + i);
+            System.out.println("Durchlauf der großen Möhren: " + i);
         }
     }
     }
