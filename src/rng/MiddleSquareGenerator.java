ba4a55e37fd956b571f182b07b4424b7c0016b41
Marvin <marvin.siegel@campus-schwarzwald.de>
2026-06-15 18:21:39 +0200

    Attributes und Getter

--- src/rng/MiddleSquareGenerator.java
+++ src/rng/MiddleSquareGenerator.java
@@ -1,5 +1,13 @@
 package rng;
 
-public class MiddleSquareGenerator {
+public class MiddleSquareGenerator implements RNG {
 
+    //Attributes
+    private static final String NAME = "Middle-Square Generator";
+
+    //Getter
+    @Override
+    public String getName() {
+        return NAME;
+    }
 }
