689e0deff2cee8715da831b5031d4f57505139e0
Marvin <marvin.siegel@campus-schwarzwald.de>
2026-06-28 17:26:49 +0200

    update

--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -73,7 +73,7 @@ public class MainFrame extends JFrame {
 
             SimulationResult result = SimulationController.runSimulation(
                     controllerGeneratorName,
-                    seed,
+                    //seed,
                     amount,
                     bins
             );
